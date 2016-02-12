f5fbb22310d9b7a71f13b47a4d8f82a096ce16b4
Hektor Wallin <hektorw@gmail.com>
2016-02-12 16:46:41 +0100

    Added BtcNdef to index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
 	BitcoinUtils: require('./src/BitcoinUtils'),
+	BtcNdef: require('./src/BtcNdef'),
 	Constants: require('./src/Constants'),
 	Crypto: require('./src/Crypto')
 };
